cea05d61b74710571941080d2b2de2a9e78baf64
Ori <OriBennett@gmail.com>
2024-01-24 10:35:08 +0200

    pnl calc

--- account_mgr.js
+++ account_mgr.js
@@ -1,6 +1,14 @@
-
+import Board from './board.js'
 
 class AccountMgr {
+
+    static _SYMBOL_VALUES = {
+        A: 20,
+        B: 15,
+        C: 10,
+        D: 5
+    }
+
     constructor(balance, board) {
         this.balance = balance;
         this.board = board;
@@ -15,5 +23,12 @@ class AccountMgr {
         let pnl = -1 * this.bet * this.numberOfLines;
 
         const wins = this.board.checkWins();
+        for (const symbol of wins) {
+            pnl += this.bet * (AccountMgr._SYMBOL_VALUES[symbol] + 1);
+        }
+
+        this.balance += pnl;
+        return pnl;
     }
+
 }
